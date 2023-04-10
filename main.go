b7fbaaf960af7529c6313ff9355672103c2ae774
Sanjula Dulshan <sdulshan10@gmail.com>
2023-04-10 22:08:43 +0530

    implement createMovie

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -23,16 +25,27 @@ type Director struct {
 
 var movies []Movie
 
+// create a movie
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+
+}
+
 // get all movies
 func getAllMovies(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-Type", "application/json") //convert struct to json
+	w.Header().Set("Content-Type", "application/json") //convert struct to json
 	json.NewEncoder(w).Encode(movies)                  //encode the movies to json
 
 }
 
 // get a movie by id
 func getMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for _, movie := range movies {
 		if movie.ID == params["id"] {
@@ -45,7 +58,7 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 
 // Delete a movie
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, movie := range movies {
 		if movie.ID == params["id"] {
